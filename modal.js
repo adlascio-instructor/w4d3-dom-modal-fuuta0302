df872d687d76110971ff03cf4825732a0e8797f6
fuuta <fkkz0302@icloud.com>
2023-03-02 07:51:56 -0800

    exercise

--- modal.js
+++ modal.js
@@ -7,3 +7,21 @@ console.log("here");
 
 // add event listener on key press Escape
 // add class invisible to div modal
+
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  alert("Hello World!");
+});
+
+let class1 = document.querySelector("modal-wrapper invisible fixed bg-gray-500 w-screen h-screen flex items-center justify-center")
+class1.classList.remove("modal-wrapper invisible fixed bg-gray-500 w-screen h-screen flex items-center justify-center")
+
+const input = document.querySelector("input");
+input.addEventListener("keypress", (event) => {
+  console.log(event.key);
+});
+
+let class2 = document.querySelector("modal-wrapper invisible fixed bg-gray-500 w-screen h-screen flex items-center justify-center")
+class2.classList.add("modal-wrapper invisible fixed bg-gray-500 w-screen h-screen flex items-center justify-center")
+
